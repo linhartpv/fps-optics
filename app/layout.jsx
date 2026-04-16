27402e2dc1ce39b8459e816eafee398038f660f5
linhartpv <linhartpv@gmail.com>
2026-04-16 16:06:39 +0200

    Create layout.jsx

---
+++ app/layout.jsx
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "FPS Optics",
+  description: "Advanced Optical Engineering & Sensing",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
